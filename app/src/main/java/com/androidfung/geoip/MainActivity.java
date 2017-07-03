448fde80ea91d8db44b487c640c4444d96073eee
Fung <funglam@astri.org>
2017-07-03 15:31:45 +0800

    drops support for Volley

--- app/src/main/java/com/androidfung/geoip/MainActivity.java
+++ app/src/main/java/com/androidfung/geoip/MainActivity.java
@@ -1,6 +1,7 @@
 package com.androidfung.geoip;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.androidfung.geoip.databinding.ActivityMainBinding;
 import com.androidfung.geoip.model.GeoIpResponseModel;
 
 import retrofit2.Call;
@@ -22,19 +22,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
-//        final TextView textViewInfo = (TextView) findViewById(R.id.textview_info);
 
         IpApiService ipApiService = ServicesManager.getGeoIpService();
 
         ipApiService.getGeoIp().enqueue(new Callback<GeoIpResponseModel>() {
             @Override
             public void onResponse(Call<GeoIpResponseModel> call, retrofit2.Response<GeoIpResponseModel> response) {
-                binding.setResponse(response.body());
+                binding.setVariable(com.androidfung.geoip.BR.response, response.body());
+
             }
 
             @Override
@@ -42,23 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), t.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-
-//        ApiManager apiManager = new ApiManager(Volley.newRequestQueue(this));
-//        apiManager.getGeoIpInfo(new Response.Listener<GeoIpResponseModel>() {
-//            @Override
-//            public void onResponse(GeoIpResponseModel response) {
-//                Log.d(TAG, String.valueOf(response==null));
-////                textViewInfo.setText(response.toString());
-//                binding.setResponse(response);
-////                binding.setVariable("response", response);
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-////                textViewInfo.setText(error.toString());
-//                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
 
     }
 
